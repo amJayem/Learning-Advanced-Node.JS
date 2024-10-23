f6c2b7d2884895da8845ba9cd1cb1f70c3d1cc16
amjayem <csejayem19@gmail.com>
2024-10-23 23:35:17 +0600

    storybook build

---
+++ storybook-static/assets/Button.stories-CrYF3LlA.js
@@ -0,0 +1,27 @@
+import{B as z}from"./Button-Bf7kBmkp.js";import"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const k=()=>{console.log("hello")},E={title:"Example/Button",component:z,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:k}},r={args:{primary:!1,label:"Button",size:"large",backgroundColor:"#c22a2a"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}},s={args:{primary:!1,label:"Button"}};var t,n,l;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: 'Button',
+    size: 'large',
+    backgroundColor: '#c22a2a'
+  }
+}`,...(l=(n=r.parameters)==null?void 0:n.docs)==null?void 0:l.source}}};var c,m,p;a.parameters={...a.parameters,docs:{...(c=a.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...(p=(m=a.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var u,g,i;e.parameters={...e.parameters,docs:{...(u=e.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...(i=(g=e.parameters)==null?void 0:g.docs)==null?void 0:i.source}}};var d,b,B;o.parameters={...o.parameters,docs:{...(d=o.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...(B=(b=o.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};var y,S,f;s.parameters={...s.parameters,docs:{...(y=s.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: 'Button'
+  }
+}`,...(f=(S=s.parameters)==null?void 0:S.docs)==null?void 0:f.source}}};const L=["Primary","Secondary","Large","Small","Test"];export{e as Large,r as Primary,a as Secondary,o as Small,s as Test,L as __namedExportsOrder,E as default};
